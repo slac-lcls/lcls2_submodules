39938b5ac12d7144063636fea9f15027369cbff2
monarin <monarin@gmail.com>
2025-10-02 12:08:21 -0700

    submodules: add epix-quad-1kfps@v2.1.3, bump epix-quad@v1.3.1, patch rogue minVersion

--- patch.sh
+++ patch.sh
@@ -1,2 +1,5 @@
 sed -i '25s/^rogue.Version.exactVersion/#rogue.Version.exactVersion/' high-rate-encoder-dev/firmware//python//high_rate_encoder_dev/_Root.py
 sed -i '313s/ raise ValueError(errMsg)/#raise ValueError(errMsg)/' cameralink-gateway/firmware//python/cameralink_gateway/_ClinkDevRoot.py 
+# Patch lcls2-pgp-pcie-apps minVersion
+sed -i "s/^rogue.Version.minVersion('6.4.0')/#rogue.Version.minVersion('6.4.0')/" \
+          lcls2-pgp-pcie-apps/firmware/python/lcls2_pgp_pcie_apps/_DevRoot.py
