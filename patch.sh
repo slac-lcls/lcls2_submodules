f3268923ff37728f4ed9029f541c59206637cc4a
Andrei A. Salnikov psrel <psrel@sdfiana024.sdf.slac.stanford.edu>
2026-03-12 18:22:04 -0700

    working changes for epix100

--- patch.sh
+++ patch.sh
@@ -1,6 +1,7 @@
-sed -i '25s/^rogue.Version.exactVersion/#rogue.Version.exactVersion/' high-rate-encoder-dev/firmware//python//high_rate_encoder_dev/_Root.py
-sed -i '313s/ raise ValueError(errMsg)/#raise ValueError(errMsg)/' cameralink-gateway/firmware//python/cameralink_gateway/_ClinkDevRoot.py 
-sed -i '231s/deps[0].set(rawVal, write=write))/deps[0].set(rawVal, write=write)/' epix-100a-gen2/software/python/ePixFpga/_ePixFpga.py
+sed -i '25s/^rogue.Version.exactVersion/#rogue.Version.exactVersion/' high-rate-encoder-dev/firmware/python/high_rate_encoder_dev/_Root.py
+sed -i '313s/ raise ValueError(errMsg)/#raise ValueError(errMsg)/' cameralink-gateway/firmware/python/cameralink_gateway/_ClinkDevRoot.py 
+
+sed -i "s/write=write))/write=write)/" epix-100a-gen2/software/python/ePixFpga/_ePixFpga.py
 sed -i '233s/raise ValueError(errMsg)/#raise ValueError(errMsg)/' lcls2-epix-hr-pcie/firmware/submodules/axi-pcie-core/python/axipcie/_AxiPcieCore.py
 # Patch lcls2-pgp-pcie-apps and cameralink-gateway minVersion
 sed -i "s/^rogue.Version.minVersion('6.4.0')/#rogue.Version.minVersion('6.4.0')/" \
@@ -8,4 +9,5 @@ sed -i "s/^rogue.Version.minVersion('6.4.0')/#rogue.Version.minVersion('6.4.0')/
 sed -i "s/^rogue.Version.minVersion('6.4.0')/#rogue.Version.minVersion('6.4.0')/" \
           cameralink-gateway/firmware/python/cameralink_gateway/_ClinkDevRoot.py
 sed -i "s/ZmqServer(root=self, addr='127.0.0.1', port=10000)/ZmqServer(root=self, addr='127.0.0.1', port=0)/" \
-	  l2si-drp/firmware/python/l2si_drp/_Root.py 
+	  l2si-drp/firmware/python/l2si_drp/_Root.py
+
